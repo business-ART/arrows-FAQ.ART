ed101ff28b24c1a306bae096ac4124d4b2813da7
ART <business.with.art33@gmail.com>
2026-02-24 23:55:57 +0100

    Update test-index.js
    Removed unnecessary lines and cleaned up event listener.

--- test-index.js
+++ test-index.js
@@ -3,6 +3,4 @@
 
 
 document.querySelector('.arrow-1').addEventListener('click', function () {
-  document.querySelector('.answer-1').classList.add('hidden-1')
-  document.querySelector('.answer-1').classList.remove('hidden-1')
 });
